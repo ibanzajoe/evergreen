272f8f294aa0fe2a8421b3d468f14b6880619ae2
dennislee <ibanzajoe@gmail.com>
2017-09-24 22:38:57 -0700

    made fontsize bigger, made footer bar fullwidth

--- pages/everwill/index.vue
+++ pages/everwill/index.vue
@@ -1,121 +1,124 @@
 <template>
-  <div class="main">
-    <div class="hero-top is-fullwidth">
-      <div class="inner">
-        <div class="top_bar">
-          <img src="/assets/img/logo.jpg" alt="everwill logo" />
-          <h3 class="is-hidden-mobile">Call Us Now <strong>(888) 888-8888</strong></h3>
-        </div>
-        <div class="left-side-content">
-          <h1>Attorney-Crafted Wills & Trusts from the Comfort of Your Home</h1>
-          <h2>No hourly billing. Unlimited revisions for life.</h2>
-          <form class="form has-text-left">
-            <div class="input_button_wrap">
-              <input class="input " placeholder="Name"/>
-              <input class="input " placeholder="Phone" />
-              <button class="button  is-danger">Learn More</button>
-              <h2 class="is-hidden-tablet callus">Call Us Now <strong>(888) 888-8888</strong></h2>
-            </div>
-          </form>
+  <div>
+    <div class="main">
+      <div class="hero-top is-fullwidth">
+        <div class="inner">
+          <div class="top_bar">
+            <img src="/assets/img/logo.jpg" alt="everwill logo" />
+            <h3 class="is-hidden-mobile">Call Us Now <strong>(888) 888-8888</strong></h3>
+          </div>
+          <div class="left-side-content">
+            <h1>Attorney-Crafted Wills & Trusts from the Comfort of Your Home</h1>
+            <h2>No hourly billing. Unlimited revisions for life.</h2>
+            <form class="form has-text-left">
+              <div class="input_button_wrap">
+                <input class="input " placeholder="Name"/>
+                <input class="input " placeholder="Phone" />
+                <button class="button  is-danger">Learn More</button>
+                <h2 class="is-hidden-tablet callus">Call Us Now <strong>(888) 888-8888</strong></h2>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="who-we-are inner">
-      <h2 class="second">Who we are</h2>
-      <h3>Duo nemore assentior definitionem ut, id appetere verterem mea. Duo aeque iustro elitr cu, duo te diam iriure copiosae.</h3>
+      <div class="who-we-are inner">
+        <h2 class="second">Who we are</h2>
+        <h3>Duo nemore assentior definitionem ut, id appetere verterem mea. Duo aeque iustro elitr cu, duo te diam iriure copiosae.</h3>
 
-      <div class="profile_wrap has-text-left">
-        <div class="columns">
-          <div class="column is-6">
-            <div class="content_wrap">
-              <img src="/assets/img/mobile-jeff-sydney.jpg" alt="Jeff Sydney" />
-              <div class="word_wrap jeff">
-                <h2>Jeff Sydney</h2>
-                <h3>Senior Associate</h3>
-                <p>Estate Planning is a relationship, not a transaction, and that's what Janice values most
-                  about her work... <a @click="handleModal(true)">Read More</a></p>
+        <div class="profile_wrap has-text-left">
+          <div class="columns">
+            <div class="column is-6">
+              <div class="content_wrap">
+                <img src="/assets/img/mobile-jeff-sydney.jpg" alt="Jeff Sydney" />
+                <div class="word_wrap jeff">
+                  <h2>Jeff Sydney</h2>
+                  <h3>Senior Associate</h3>
+                  <p>Estate Planning is a relationship, not a transaction, and that's what Janice values most
+                    about her work... <a @click="handleModal(true)">Read More</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="column is-3">
+              <div class="content_wrap">
+                <img src="/assets/img/mobile-peng-li.jpg" alt="Peng Li" />
+                <div class="word_wrap peng">
+                  <h2>Peng Li</h2>
+                  <h3>Senior Associate</h3>
+                  <p>Estate Planning is a relationship, not a transaction, and that's what Janice values most
+                    about her work... <a @click="handleModal(true)">Read More</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="column is-3">
+              <div class="content_wrap">
+                <img src="/assets/img/mobile-janice-shen.jpg" alt="Janice Shen" />
+                <div class="word_wrap janice">
+                  <h2>Janice Shen</h2>
+                  <h3>Senior Associate</h3>
+                  <p>Estate Planning is a relationship, not a transaction, and that's what Janice values most
+                    about her work... <a @click="handleModal(true)">Read More</a></p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="reason inner">
+        <h2 class="second">3 More Reasons to Choose Everwill</h2>
+
+        <div class="wrap">
+          <div class="columns is-centered">
+            <div class="column is-4">
+              <div class="reason_wrap">
+                <img class="is-hidden-mobile" src="/assets/img/safe.jpg" alt="safe" />
+                <img class="is-hidden-tablet" src="/assets/img/mobile-safe.jpg" alt="safe" />
+                <h2><strong>Never lose your will</strong></h2>
+                <h3></h3>
+                <p>We store it for you in a bank-grade vault</p>
+              </div>
+            </div>
+            <div class="column is-4 larger">
+              <div class="reason_wrap">
+                <img class="is-hidden-mobile" src="/assets/img/contract.jpg" alt="safe" />
+                <img class="is-hidden-tablet" src="/assets/img/mobile-secure-lock.jpg" alt="safe" />
+                <h2><strong>Allow doctors' emergency access to your end-of-life wishes</strong></h2>
+                <h3></h3>
+                <p>Carry your Everwill identification Card in your wallet</p>
+              </div>
+            </div>
+            <div class="column is-4">
+              <div class="reason_wrap">
+                <img class="is-hidden-mobile" src="/assets/img/contract_shield.jpg" alt="safe" />
+                <img class="is-hidden-tablet" src="/assets/img/mobile-contract.jpg" alt="safe" />
+                <h2><strong>Prevent unauthorized access to your will</strong></h2>
+                <h3></h3>
+                <p>Manage who gets access and when from our online portal</p>
               </div>
             </div>
           </div>
-          <div class="column is-3">
-            <div class="content_wrap">
-              <img src="/assets/img/mobile-peng-li.jpg" alt="Peng Li" />
-              <div class="word_wrap peng">
-                <h2>Peng Li</h2>
-                <h3>Senior Associate</h3>
-                <p>Estate Planning is a relationship, not a transaction, and that's what Janice values most
-                  about her work... <a @click="handleModal(true)">Read More</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="column is-3">
-            <div class="content_wrap">
-              <img src="/assets/img/mobile-janice-shen.jpg" alt="Janice Shen" />
-              <div class="word_wrap janice">
-                <h2>Janice Shen</h2>
-                <h3>Senior Associate</h3>
-                <p>Estate Planning is a relationship, not a transaction, and that's what Janice values most
-                  about her work... <a @click="handleModal(true)">Read More</a></p>
-              </div>
-            </div>
-
-          </div>
         </div>
       </div>
+
+      <div class="footer_call">
+        <div class="call_now inner">
+          <h2 class="second">Get started now!</h2>
+          <h2 class="second light-grey">Call us at 888-383-7945</h2>
+          <button class="button is-medium is-danger">Contact Us Today</button>
+        </div>
+      </div>
+
     </div>
+    <div class="bottom_bar ">
+      <div class="links inner">
+        <span class="is-hidden-mobile"><small>2017 Everwill. All rights reserved</small></span>
+        <a>Privacy Policy</a>
+        <a>Terms of Use</a>
+        <a>Attorney Advertising</a>
+        <p><span class="is-hidden-tablet"><small>2017 Everwill. All rights reserved</small></span></p>
+        <img src="/assets/img/mobile-bbb-logo.jpg" alt="logo" />
 
-    <div class="reason inner">
-      <h2 class="second">3 More Reasons to Choose Everwill</h2>
-
-      <div class="wrap">
-        <div class="columns is-centered">
-          <div class="column is-4">
-            <div class="reason_wrap">
-              <img class="is-hidden-mobile" src="/assets/img/safe.jpg" alt="safe" />
-              <img class="is-hidden-tablet" src="/assets/img/mobile-safe.jpg" alt="safe" />
-              <h2><strong>Never lose your will</strong></h2>
-              <h3></h3>
-              <p>We store it for you in a bank-grade vault</p>
-            </div>
-          </div>
-          <div class="column is-4">
-            <div class="reason_wrap">
-              <img class="is-hidden-mobile" src="/assets/img/contract.jpg" alt="safe" />
-              <img class="is-hidden-tablet" src="/assets/img/mobile-secure-lock.jpg" alt="safe" />
-              <h2><strong>Allow doctors' emergency access to your end-of-life wishes</strong></h2>
-              <h3></h3>
-              <p>Carry your Everwill identification Card in your wallet</p>
-            </div>
-          </div>
-          <div class="column is-4">
-            <div class="reason_wrap">
-              <img class="is-hidden-mobile" src="/assets/img/contract_shield.jpg" alt="safe" />
-              <img class="is-hidden-tablet" src="/assets/img/mobile-contract.jpg" alt="safe" />
-              <h2><strong>Prevent unauthorized access to your will</strong></h2>
-              <h3></h3>
-              <p>Manage who gets access and when from our online portal</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="footer_call">
-      <div class="call_now inner">
-        <h2 class="second">Get started now!</h2>
-        <h2 class="second light-grey">Call us at 888-383-7945</h2>
-        <button class="button is-medium is-danger">Contact Us Today</button>
-      </div>
-      <div class="bottom_bar ">
-        <div class="links inner">
-          <span class="is-hidden-mobile"><small>2017 Everwill. All rights reserved</small></span>
-          <a>Privacy Policy</a>
-          <a>Terms of Use</a>
-          <a>Attorney Advertising</a>
-          <p><span class="is-hidden-tablet"><small>2017 Everwill. All rights reserved</small></span></p>
-          <img src="/assets/img/mobile-bbb-logo.jpg" alt="logo" />
-
-        </div>
       </div>
     </div>
     <div class="profile_modal shadow_layer" v-if="modalToggle == true">
@@ -177,9 +180,8 @@
       </div>
 
     </div>
-
-
   </div>
+
 </template>
 <script type="text/ecmascript-6">
 
@@ -200,35 +202,36 @@
   @import '~assets/css/variables.scss';
 
   h1 {
-    font-size: 32px;
+    font-size: 34px;
+    line-height: 1.2;
     color: $black;
   }
   h2 {
-    font-size: 12px;
+    font-size: 16px;
     font-weight: 700;
     color: $light-grey;
     &.second {
-      font-size: 22px;
+      font-size: 24px;
       font-weight: 500;
       color: $black;
     }
     @media screen and (max-width: 768px) {
-      font-size: 17px;
+      font-size: 18px;
       &.second {
-        font-size: 24px;
+        font-size: 26px;
         font-weight: 600;
       }
     }
   }
   h3 {
-    font-size: 10px;
+    font-size: 14px;
     color: $light-grey;
     @media screen and (max-width: 768px) {
-      font-size: 16px;
+      font-size: 18px;
     }
   }
   p {
-    font-size: 8px;
+    font-size: 12px;
   }
 
 
@@ -284,20 +287,24 @@
       font-weight: 700;
     }
     .form {
-      margin-top: 1rem;
+      margin-top: 1.2rem;
       .input_button_wrap {
         display: flex;
         align-items: center;
         justify-content: space-between;
+        .input {
+          height: $main-button-height;
+          border-radius: 50;
+        }
         .button {
           display: flex;
           align-self: flex-start;
           background-color: $red;
           padding-left: 2rem;
           padding-right: 2rem;
-          font-size: 8px;
+          font-size: 12px;
           line-height: 1.5;
-          height: 32px;
+          height: $main-button-height;
           border-radius: 5px;
         }
         .input + .input {
@@ -371,19 +378,19 @@
             max-width: 280px;
             text-align: left;
             h2 {
-              font-size: 12px;
+              font-size: 14px;
               font-weight: 500;
               color: #ffffff;
             }
             h3 {
-              font-size: 10px;
+              font-size: 12px;
               font-weight: 300;
               margin-top: 0.20rem;
               color: #ffffff;
             }
             p {
               margin-top: 1rem;
-              font-size: 8px;
+              font-size: 10px;
               color: #ffffff;
               a {
                 color: $red;
@@ -414,8 +421,14 @@
         .column{
           &.is-4 {
             width: 30%;
+            &.larger {
+              width: 35%;
+            }
             @media screen and (max-width: 768px) {
               width: 100%;
+              &.larger {
+                width: 100%;
+              }
             }
           }
         }
@@ -426,6 +439,7 @@
         h2 {
           margin-top: 1.5rem;
           strong {
+            font-weight: 500;
             color: $light-dark-grey;
           }
           color: $light-dark-grey;
@@ -462,7 +476,7 @@
     background-repeat: no-repeat;
     background-position: right;
     background-position-y: -10px;
-    padding-top: 90px;
+    padding-top: 75px;
 
     display: flex;
     flex-direction: column;
@@ -478,18 +492,15 @@
         padding-left: 2.5rem;
         padding-right: 2.5rem;
         margin-top: 2rem;
-        font-size: 8px;
+        font-size: 12px;
         line-height: 1.5;
-        height: 32px;
+        height: $main-button-height;
         border-radius: 5px;
       }
     }
-    .bottom_bar {
-      background-color: rgba(22, 67, 73, 0.85);
-    }
     @media screen and (max-width: 768px) {
       background-image: url('/assets/img/background_mobile_family_bot.jpg');
-      background-position-y: 60%;
+      background-position-y: 85%;
       padding-top: 0rem;
       margin-top: 100px;
       .call_now {
@@ -499,9 +510,10 @@
     }
   }
   .bottom_bar {
+    background-color: rgba(22, 67, 73, 0.85);
     padding-top: 0.5rem;
     padding-bottom: 0.5rem;
-
+    width: 100%;
     .links {
       display: flex;
       justify-content: center;
